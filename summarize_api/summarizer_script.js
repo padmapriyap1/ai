728e1fe5b781d08df753c37fd964f1fc617b7a4f
Priya Palanisamy <priyapal@microsoft.com>
2025-03-12 13:09:51 -0700

    Clean up the script with latest changes with upstream

--- summarize_api/summarizer_script.js
+++ summarize_api/summarizer_script.js
@@ -47,34 +47,27 @@ const n = document.querySelector("#input")
         type: r,
         format: i,
         length: o,
-        monitor(m) {
-            m.addEventListener("downloadprogress", e => {
-            // update the progress bar with latest download status
-            document.getElementById("modelDownloadProgress").value = (e.loaded / e.total) * 100;
-            if (e.loaded == e.total) {
-                document.getElementById("modelDownloadProgress").value = 100;
-                console.log("Download complete");
-            }
+        monitor: e
             });
-        }
-    })
-} , cd = async () => {
+  }
+  , cd = async () => {
       let result = await window.ai.summarizer.availability();
-      if (result.available == 'after-download')
+      if (result == 'after-download')
       {
           window.setTimeout(cd, 1000);
       }
-      if (result.available == 'readily')
+      if (result == 'available')
       {
           window.location.reload();
       }
-  }, y = async () => {
+ }
+ , y = async () => {
     let r = await window.ai.summarizer.availability();
-    if (r.available === "readily") {
+    if (r === "available") {
         document.getElementById("modelDownloadProgress").value = 100;
         return !0;
     }
-    if(r.available === "after-download"){
+    if(r === "after-download"){
         cd();
         return !0;
     }
@@ -97,9 +90,9 @@ const n = document.querySelector("#input")
         })
     } catch {}
     return r = await window.ai.summarizer.availability(),
-    r.available !== "no"
-}
-  , S = async () => {
+    r !== "no"
+ }
+ , S = async () => {
     if (!(window.ai !== void 0 && window.ai.summarizer !== void 0)) {
         h.style.display = "block";
         return
